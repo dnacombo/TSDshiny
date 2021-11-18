d8958c3ff0a8f20f4b37cc553c1b468323d9a2cc
Maximilien Chaumon <maximilien.chaumon@gmail.com>
2021-11-18 10:22:23 +0100

    Add GNU GPL license

--- app.R
+++ app.R
@@ -1,11 +1,23 @@
+# This is the Blursday database server source code.
+# The live application can be accessed at 
+# https://dnacombo.shinyapps.io/Blursday/
+# This code is publicly available at
+# https://github.com/dnacombo/TSDshiny
+
+#     Copyright (C) 2021  Maximilien Chaumon
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
 #
-# Find out more about building applications with Shiny here:
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
 #
-#    http://shiny.rstudio.com/
-#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 library(shiny)
 library(shinyjs)
